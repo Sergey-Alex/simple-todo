9627f0db2eab517ee08b7a2b634c557091a25b51
Sergey <stroitelevsergeyalex@gmail.com>
2023-06-12 19:18:03 +0300

    add test for todolist-reducer

--- src/tests/todolist-reducer.test.ts
+++ src/tests/todolist-reducer.test.ts
@@ -1,7 +1,8 @@
 import {StateType, todolistReducers} from "../reducer/TodolistReducers";
+import {v1} from "uuid";
 
 
-test('correct todolist should be added', () => {
+test('todolist should be added task', () => {
     const startState :StateType = {
         tasks: [],
         filter: "all"
@@ -11,5 +12,41 @@ test('correct todolist should be added', () => {
 
     expect(endState.tasks[0].title).toBe('Hello World')
     expect(endState.tasks[0].check).toBe(false)
+    expect(endState.filter).toBe('all')
+    expect(endState.tasks.length).toBe(1)
 
 })
+test('task status change checked', () => {
+    let id1 = v1()
+    let id2 = v1()
+    const startState :StateType = {
+        tasks: [
+            {id: id1, title:'1', check: false},
+            {id: id2, title:'2', check: false}],
+        filter: "all"
+    }
+
+    const endState = todolistReducers(startState, {type: "CHECK",check: true, id: id1})
+
+    expect(endState.tasks[0].check).toBe(true)
+    expect(endState.tasks[1].check).toBe(false)
+    expect(endState.filter).toBe('all')
+    expect(endState.tasks.length).toBe(2)
+
+})
+
+test('todolist filter should be changed', () => {
+    const startState :StateType = {
+        tasks: [],
+        filter: "all"
+    }
+
+    const endState = todolistReducers(startState, {type: "FILTER", filter: "active"})
+
+
+    expect(endState.filter).toBe('active')
+    expect(endState.tasks.length).toBe(0)
+
+})
+
+
